703f48b1a20dc0e0d524fd765be32f896f2ab85f
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-07-05 03:21:39 +0300

    addinп booking of place

--- src/components/Places/Places.jsx
+++ src/components/Places/Places.jsx
@@ -1,12 +1,46 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { setPlacesFilm } from "../../store/dataSlices"
 import styles from "./Places.module.scss"
 
 function Places() {
+    const dispatch = useDispatch()
     const data = useSelector((state) => state.dataOfFilms)
     const keys = Object.keys(data.placesFilm)
     const values = Object.values(data.placesFilm)
 
     console.log(data.placesFilm)
+
+    const clickOnPlace = (index) => {
+        if (values[index] === true) {
+            // change true on false
+            values[index] = false
+
+            // unite keys and values in object
+            const result = keys.reduce((obj, key, index) => {
+                obj[key] = values[index]
+                return obj
+            }, {})
+
+            // console.log(result)
+
+            // assign result to placesFilm
+            dispatch(setPlacesFilm(result))
+        } else {
+            // change true on false
+            values[index] = true
+
+            // unite keys and values in object
+            const result = keys.reduce((obj, key, index) => {
+                obj[key] = values[index]
+                return obj
+            }, {})
+
+            // console.log(result)
+
+            // assign result to placesFilm
+            dispatch(setPlacesFilm(result))
+        }
+    }
 
     return (
         <div>
@@ -14,10 +48,14 @@ function Places() {
                 <button
                     style={{ backgroundColor: values[index] ? "green" : "red" }}
                     key={index}
+                    onClick={() => {
+                        clickOnPlace(index)
+                    }}
                 >
                     {item}
                 </button>
             ))}
+
             {/* {values.map(item => <h3>{it em}</h3>)} */}
         </div>
     )
