43987968d66f3a19bcb7fa0c5e2eced6a653ce50
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-07-06 23:58:40 +0300

    (3) part of booking places(logica)

--- src/components/Places/Places.jsx
+++ src/components/Places/Places.jsx
@@ -1,117 +1,3 @@
-// import { useDispatch, useSelector } from "react-redux"
-// import { setPlacesFilm, setAllFilms } from "../../store/dataSlices"
-// import styles from "./Places.module.scss"
-// import { useEffect } from "react"
-// import axios from "axios"
-
-// function Places() {
-//     const dispatch = useDispatch()
-//     const data = useSelector((state) => state.dataOfFilms)
-
-//     // ---------------------------------------------------------------------------------------------
-
-//     // Получаем все фильмы с mockApi
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             try {
-//                 const allFilms = await axios.get(
-//                     "https://6478d572362560649a2e842a.mockapi.io/cinema"
-//                 )
-//                 localStorage.setItem(
-//                     "allFilmsLocal",
-//                     JSON.stringify(allFilms.data)
-//                 )
-//                 dispatch(setAllFilms(allFilms.data)) // Сохраняем все фильмы в состоянии
-//             } catch (error) {
-//                 console.error(error)
-//             }
-//         }
-
-//         fetchData()
-//     }, [])
-
-//     // инфа которую выбрал пользователь
-//     // название - дата - время
-//     // infoForPlacesLocal[0] - infoForPlacesLocal[1] - infoForPlacesLocal[2]
-
-//     const infoForPlacesLocal = JSON.parse(
-//         localStorage.getItem("infoForPlacesLocal")
-//     )
-//     console.log(infoForPlacesLocal)
-
-//     const allFilmsLocal = JSON.parse(localStorage.getItem("allFilmsLocal"))
-
-//     console.log(allFilmsLocal)
-
-//     // Проверяем наличие данных в allFilmsLocal
-//     if (!allFilmsLocal) {
-//         return <div>Loading...</div> // Отображаем загрузку или другой индикатор ожидания
-//     }
-
-//     const clickOnPlace = (event, keys) => {
-
-//         // определяю число
-//         const textContentBtn = event.target.textContent
-//         console.log(textContentBtn)
-//         // определяю индекс числа в массиве ключей
-//         const indexBtn =  keys.indexOf(textContentBtn)
-//         console.log(indexBtn)
-//     }
-
-//     return (
-//         <div className={styles.container}>
-//             <div className={styles.containerBtn}>
-//                 {allFilmsLocal.map((item) => {
-//                     // true/false
-//                     const dateFilm = item.dates.includes(infoForPlacesLocal[1])
-//                     const indexDateFilm = item.dates.indexOf(
-//                         infoForPlacesLocal[1]
-//                     )
-
-//                     const arrTimes = item.time[indexDateFilm]
-//                     // true/false
-//                     const timeFilm = arrTimes.includes(infoForPlacesLocal[2])
-//                     const indexTimeFilm = arrTimes.indexOf(
-//                         infoForPlacesLocal[2]
-//                     )
-
-//                     if (
-//                         item.name === infoForPlacesLocal[0] &&
-//                         dateFilm &&
-//                         timeFilm
-//                     ) {
-//                         // return <h1 key={index}>Well!</h1>
-
-//                         const keys = Object.keys(
-//                             item.places[indexDateFilm][indexTimeFilm]
-//                         )
-//                         // console.log(keys)
-
-//                         const values = Object.values(
-//                             item.places[indexDateFilm][indexTimeFilm]
-//                         )
-
-//                         console.log(values)
-//                         return keys.map((item, index) => {
-//                             const indexOfItem = keys.indexOf(item);
-//                             const colorBtn = values[indexOfItem] ? "green" : "red";
-
-//                             return (
-//                                 <button key={index} className={`${styles.button} ${styles[colorBtn]}`} onClick={(event, keys) => {clickOnPlace(event, keys)}}>
-//                                     {item}
-//                                 </button>
-//                             );
-//                         });
-
-//                     }
-//                 })}
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Places
-
 import { useDispatch, useSelector } from "react-redux"
 import { setPlacesFilm, setAllFilms } from "../../store/dataSlices"
 import styles from "./Places.module.scss"
@@ -173,9 +59,6 @@ function Places() {
         values[indexBtn] = !values[indexBtn]
         console.log(values[indexBtn])
 
-        // const keys = ["key1", "key2", "key3"]
-        // const values = ["value1", "value2", "value3"]
-
         // objPlaces - объединенный объект из ключей и значений
         const objPlaces = keys.reduce((result, key, index) => {
             result[key] = values[index]
@@ -189,11 +72,9 @@ function Places() {
 
         if (allFilmsLocal) {
             allFilmsLocal.map((item) => {
-                // const dateFilm = item.dates.includes(infoForPlacesLocal[1])
                 const indexDateFilm = item.dates.indexOf(infoForPlacesLocal[1])
 
                 const arrTimes = item.time[indexDateFilm]
-                // const timeFilm = arrTimes.includes(infoForPlacesLocal[2])
                 const indexTimeFilm = arrTimes.indexOf(infoForPlacesLocal[2])
 
                 if (
@@ -201,7 +82,7 @@ function Places() {
                     item.dates.includes(infoForPlacesLocal[1]) &&
                     item.time[indexDateFilm].includes(infoForPlacesLocal[2])
                 ) {
-                    // корректируем allFilmsLocal - меняем в нем старый объект 
+                    // корректируем allFilmsLocal - меняем в нем старый объект
                     // на новый - с обновленной информацие о местах
                     item.places[indexDateFilm][indexTimeFilm] = objPlaces
                 }
@@ -209,6 +90,26 @@ function Places() {
             })
 
             console.log(allFilmsLocal)
+
+            useEffect(() => {
+                const updateData = async () => {
+                  try {
+    
+            
+                    await axios.put(
+                      "https://6478d572362560649a2e842a.mockapi.io/cinema",
+                      allFilmsLocal
+                    );
+            
+                    // Дополнительный код после успешного обновления данных
+                  } catch (error) {
+                    console.error(error);
+                  }
+                };
+            
+                updateData();
+              }, []);
+            
         }
     }
 
